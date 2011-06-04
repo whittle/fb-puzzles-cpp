44d54a57c1febe964ff89702312de592b7551e38
Jason Whittle <jason.whittle@fynanz.com>
2011-06-04 17:09:58 -0400

    Submit meepmeep.

---
+++ meepmeep/src/meepmeep.cpp
@@ -0,0 +1,13 @@
+/*
+ * keyword: meepmeep
+ * puzzle: http://www.facebook.com/careers/puzzles.php?puzzle_id=3
+ * language: GNU C++ (gcc v 4.2.1)
+ * author: Jason Whittle <jason.whittle@gmail.com>
+ */
+
+#include <iostream>
+
+int main() {
+  std::cout << "Meep meep!" << std::endl;
+  return 0;
+}
